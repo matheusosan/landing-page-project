6d7a081e81a07a824c94c80df5798f4e4fc727b8
Matheus <matheussantanaoli@gmail.com>
2022-11-27 19:32:14 -0300

    Subindo projeto pro github

--- src/components/about/styles.js
+++ src/components/about/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AboutMain = styled.div `
     display: flex;
-    background-color: #ffff;
+    background-color: #fff;
     height: 90vh;
     width: 100%;
 `
