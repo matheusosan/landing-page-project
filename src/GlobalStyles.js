63cb94a7246f26c6655edcef6b7c10abc689fdaf
Matheus <matheussantanaoli@gmail.com>
2022-11-27 21:20:04 -0300

    Feito a aba de book now

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,6 +5,6 @@ export const GlobalStyle = createGlobalStyle `
         padding: 0;
         margin: 0;
         font-family: 'Poppins', sans-serif;
-        
+        box-sizing: border-box;
     }
 `
