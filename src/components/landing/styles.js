92bd0cd869e76cc9f0e2bf5a8d6c609008094a75
Matheus <matheussantanaoli@gmail.com>
2023-01-01 17:44:14 -0300

    style: ajustado estilização de links

--- src/components/landing/styles.js
+++ src/components/landing/styles.js
@@ -1,27 +1,25 @@
 import styled from "styled-components";
-import Img from '../../assets/images/landing/bg3.jpg'
+import Img from "../../assets/images/landing/bg3.jpg";
 
-export const LandingContainer = styled.div `
-    display: flex;
-    align-items: center;
-    width: 100%;
-    position: relative;
-    background-image: url(${Img});
-    background-position: center;
-    background-size: cover;
-    height: 90vh;
-    
-`
+export const LandingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  background-image: url(${Img});
+  background-position: center;
+  background-size: cover;
+  height: 90vh;
+`;
 
-export const Image = styled.img `
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    width: 819px;
-    height: 700px;
+export const Image = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 820px;
+  height: 700px;
 
-    @media (max-width: 415px) {
-            display: none;
-    }    
-    
-`
+  @media (max-width: 415px) {
+    display: none;
+  }
+`;
